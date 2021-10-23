0df7b70ef0ea7e5dcaa20a1f44a94fd88a0888ac
maheshkumar <mahesh.rose001@gmail.com>
2021-10-23 23:50:00 +0200

    ui design

--- weatherui/src/app/app.component.ts
+++ weatherui/src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   public now: Date = new Date();
-
+  city: string = "";
   constructor() {
-      setInterval(() => {
-        this.now = new Date();
-      }, 1);
+    setInterval(() => {
+      this.now = new Date();
+    }, 1);
+  }
+
+  resultPannelShow: boolean = false;
+  toggleShow() {
+    this.resultPannelShow = !this.resultPannelShow;
   }
 }
